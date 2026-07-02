20a19956ada461cfeaeaf4cd64524db69364f2d6
Jang <sdsd090811@gmail.com>
2026-07-02 15:57:26 +0900

    feat: Products 누락된 ID 코드 추가

--- src/main/java/com/example/flashsale/products/Products.java
+++ src/main/java/com/example/flashsale/products/Products.java
@@ -1,6 +1,9 @@
 package com.example.flashsale.products;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int price;
